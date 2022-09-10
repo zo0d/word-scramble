47ee3b3d44f875aa1e891489dad5cc0db9540c29
zo0d <bjahanzad@gmail.com>
2022-09-10 04:20:36 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Behzad Jahanzad on 2022-09-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/word_scrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  word_scrambleApp.swift
+//  Shared
+//
+//  Created by Behzad Jahanzad on 2022-09-10.
+//
+
+import SwiftUI
+
+@main
+struct word_scrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
